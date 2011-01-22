4a14aa219fff710edee97ae645bafcea57a21846
Kenneth Reitz <me@kennethreitz.com>
2011-01-22 18:40:23 -0500

    Basic start.

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from distutils.core import setup
+
+
+def publish():
+	"""Publish to PyPi"""
+	os.system("python setup.py sdist upload")
+
+if sys.argv[-1] == "publish":
+	publish()
+	sys.exit()
+
+required = []
+
+setup(
+	name='wolfram',
+	version='0.0.1',
+	description='Python wrapper for the Wolfram Alpha API, v2',
+	long_description=open('README.rst').read() + '\n\n' +
+	                 open('HISTORY.rst').read(),
+	author='Kenneth Reitz',
+	author_email='me@kennethreitz.com',
+	url='https://github.com/kennethreitz/wolfram',
+	packages= [
+		'wolfram',
+	],
+	install_requires=required,
+	license='MIT',
+	classifiers=(
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		# 'Programming Language :: Python :: 3.0',
+		# 'Programming Language :: Python :: 3.1',
+	),
+)
