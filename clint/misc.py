f66393aef649bb8db924758806d3085ae3c289d6
Kenneth Reitz <me@kennethreitz.com>
2011-03-19 15:22:35 -0400

    various application helpers

--- clint/misc.py
+++ clint/misc.py
@@ -7,3 +7,48 @@ class Borg(object):
         obj = object.__new__(cls, *a, **k)
         obj.__dict__ = cls._shared_state
         return obj
+
+def progressbar(it, prefix='', size=32, hide=False):
+    count = len(it)
+    if count:
+        def _show(_i):
+            x = int(size*_i/count)
+            if not hide:
+                sys.stdout.write("%s[%s>%s] %i/%i\r" % (prefix, "="*x, "-"*(size-x), _i, count))
+                sys.stdout.flush()
+
+        _show(0)
+    for i, item in enumerate(it):
+        yield item
+        _show(i+1)
+    if not hide:
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+
+def is_collection(obj):
+    """Tests if an object is a collection"""
+
+    val = hasattr(obj, '__getitem__')
+
+    if isinstance(obj, basestring):
+        return False
+
+    return val
+    
+def fixedwidth(string, width=15):
+    """Adds column to output steam."""
+    if len(string) > width:
+        return string[:width]
+    else:
+        return string.ljust(width)
+
+
+def mkdir_p(path):
+    """mkdir -p"""
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
