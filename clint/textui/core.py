96c8234c2477224063ff92dcf802b0a87a6137bc
Kenneth Reitz <me@kennethreitz.com>
2011-03-18 22:15:34 -0400

    This is just nuts.

--- clint/textui/core.py
+++ clint/textui/core.py
@@ -6,12 +6,50 @@ x
 """
 
 import sys
+from contextlib import contextmanager
+
 
 STDOUT = sys.stdout.write
 STDERR = sys.stderr.write
 
-def indent(x=4):
-    print x
+class Puts(object):
+    class Borg(object):
+        """All instances of Borg have the same value."""
+        _shared_state = {}
+        def __new__(cls, *a, **k):
+            obj = object.__new__(cls, *a, **k)
+            obj.__dict__ = cls._shared_state
+            return obj
+        def __init__(self):
+            self.indent_level = 0
+            
+    shared = Borg()
+    
+    def __init__(self, indent=0, quote=' ', indent_char=' '):
+        # self.shared = Borg()
+        self.indent = indent
+        self.indent_char = indent_char
+        self.indent_quote = quote
+        self.shared.indent_level += indent
+        
+    def __enter__(self):
+        return self
+        
+    def __exit__(self, type, value, traceback):
+        self.shared.indent_level += (-1 * self.indent)
+        self.indent_quote = ''
+        self.indent = 0
+        
+    def __call__(self, x, newline=True):
+        _str = ''.join((
+            (self.indent_char * (self.shared.indent_level - self.indent)),
+            self.indent_quote,
+            (self.indent_char * (self.indent - len(self.indent_quote))),
+            x,
+            '\n' if newline else ''
+            ))
+        STDOUT(_str)
+        
 
 
 def _out(stream, s, newline):
@@ -31,6 +69,11 @@ def puts_err(s, newline=True):
     _out(STDERR, s, newline)
 
 
+def indent(indent=4, quote=' '):
+    return Puts(indent=indent, quote=quote)
+
+
+@contextmanager
 def maxwidth(x=None):
     # if none, detect from applib
     print x
