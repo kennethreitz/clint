c7f68e33c383e7c9cae126771219a42931d6fce6
Kenneth Reitz <me@kennethreitz.com>
2011-03-19 11:43:17 -0400

    puts() cleanups

--- clint/textui/core.py
+++ clint/textui/core.py
@@ -18,7 +18,6 @@ class Puts(object):
     shared = dict(indent_level=0, indent_strings=[])
     
     def __init__(self, indent=0, quote='', indent_char=' '):
-        # self.shared = Borg()
         self.indent = indent
         self.indent_char = indent_char
         self.indent_quote = quote
@@ -30,7 +29,6 @@ class Puts(object):
         # self.shared['indent_level'] += indent
         if len(self.indent_string):
             self.shared['indent_strings'].append(self.indent_string)
-        # print self.shared['indent_strings']
         
     
         
@@ -38,32 +36,15 @@ class Puts(object):
         return self
         
     def __exit__(self, type, value, traceback):
-        # print 'exiting! %s' % len(self.shared['indent_strings'])
-        # self.shared['indent_level'] += (-1 * self.indent)
-        # self.shared['indent_level'] += (-1 * self.indent)
-        # self.shared['indent_str'] =''
-        # self.indent_quote = ''
-        # self.indent=0
-        # self.shared['indent_str'] = \
-            # self.shared['indent_str'].replace(self.indent_str, '')
-        # print len( self.shared['indent_str'])
         self.shared['indent_strings'].pop()
-        # print len( self.shared['indent_str'])
-        # print len(self.indent_str)
-        # print len(self.shared['indent_str'])
-        # print self.shared['indent_level']
-            
-        # print self.shared['indent_str']
         
     def __call__(self, s, newline=True, stream=STDOUT):
-        # print self.shared['indent_level']
         _str = ''.join((
             ''.join(self.shared['indent_strings']),
             str(s),
             '\n' if newline else ''
             ))
         stream(_str)
-        
 
 
 def _out(stream, s, newline):
