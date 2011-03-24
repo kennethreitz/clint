598deda4d9ea5c8e40202935a36aad2da115f3d0
star:Kenneth Reitz <me@kennethreitz.com>
2011-03-24 05:07:14 -0400

    python 2.5 support!

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3.0',
