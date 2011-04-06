4b9e35c631ca26d3c114a6b9251dc5aacc1506ff
star:Kenneth Reitz <me@kennethreitz.com>
2011-04-06 13:02:40 -0400

    configurable progress interator streams

--- clint/textui/progress.py
+++ clint/textui/progress.py
@@ -10,9 +10,9 @@ This module provides the progressbar functionality.
 
 from __future__ import absolute_import
 
-from .core import puts
-
 import sys
+
+STREAM = sys.stderr
 
 BAR_TEMPLATE = '%s[%s%s] %i/%i\r'
 BAR_EMPTY_CHAR = '-'
@@ -23,37 +23,46 @@ DOTS_CHAR = '.'
 
 def bar(it, label='', width=32, hide=False):
     """Progress iterator. Wrap your iterables with it."""
-    count = len(it)
-    if count:
-        def _show(_i):
-            x = int(width*_i/count)
-            if not hide:
-                puts(BAR_TEMPLATE % (
-                    label, BAR_FILLED_CHAR*x, BAR_EMPTY_CHAR*(width-x), _i, count), newline=False)
-                sys.stdout.flush()
 
+    def _show(_i):
+        x = int(width*_i/count)
+        if not hide:
+            STREAM.write(BAR_TEMPLATE % (
+                label, BAR_FILLED_CHAR*x, BAR_EMPTY_CHAR*(width-x), _i, count))
+            STREAM.flush()
+
+    count = len(it)
+
+    if count:
         _show(0)
+
     for i, item in enumerate(it):
+
         yield item
         _show(i+1)
+
     if not hide:
-        sys.stdout.write("\n")
-        sys.stdout.flush()
+        STREAM.write('\n')
+        STREAM.flush()
 
 
 def dots(it, label='', hide=False):
     """Progress iterator. Prints a dot for each item being iterated"""
-    count = len(it)
-    
-    puts(label, newline=False)
-    if count:
-        def _show(_i):
-            # sys.stdout.write('.')
-            puts(DOTS_CHAR, newline=False)
 
-        _show(0)
-    for i, item in enumerate(it):
-        yield item
-        _show(i+1)
+    count = 0
+
     if not hide:
-        sys.stdout.write("\n")
+        STREAM.write(label)
+
+    for item in it:
+        if not hide:
+            STREAM.write(DOTS_CHAR)
+            sys.stderr.flush()
+
+        count += 1
+
+        yield item
+
+    STREAM.write('\n')
+    STREAM.flush()
+
