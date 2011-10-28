8e6973253f45dc4bbf5aa489531cc70f9e0c209c
Kenneth Reitz <me@kennethreitz.com>
2011-10-28 13:04:31 -0300

    Update clint/textui/progress.py

--- clint/textui/progress.py
+++ clint/textui/progress.py
@@ -26,7 +26,7 @@ def bar(it, label='', width=32, hide=False, empty_char='-', filled_char='='):
         x = int(width*_i/count)
         if not hide:
             STREAM.write(BAR_TEMPLATE % (
-                label, bar_filled_char*x, bar_empty_char*(width-x), _i, count))
+                label, filled_char*x, empty_char*(width-x), _i, count))
             STREAM.flush()
 
     count = len(it)
