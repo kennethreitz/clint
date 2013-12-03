cab8004284a5d69b75f116276b41f9742c314060
Kenneth Reitz <me@kennethreitz.com>
2013-12-02 22:15:42 -0500

    v0.3.1

--- clint/__init__.py
+++ clint/__init__.py
@@ -26,8 +26,8 @@ from .pipes import piped_in
 
 
 __title__ = 'clint'
-__version__ = '0.3.0'
-__build__ = 0x000300
+__version__ = '0.3.1'
+__build__ = 0x000301
 __author__ = 'Kenneth Reitz'
 __license__ = 'ISC'
 __copyright__ = 'Copyright 2012 Kenneth Reitz'
