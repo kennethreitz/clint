e00476d195945625c621106b92c21195d4d8f8da
Jason Piper <j.piper@me.com>
2014-01-10 11:20:43 +0000

    optimisations for schunk and tsplit

--- clint/utils.py
+++ clint/utils.py
@@ -61,37 +61,15 @@ def mkdir_p(path):
 
 def tsplit(string, delimiters):
     """Behaves str.split but supports tuples of delimiters."""
-
     delimiters = tuple(delimiters)
-    stack = [string,]
+    if len(delimiters) < 1:
+        return [string,]
+    final_delimiter = delimiters[0]
+    for i in delimiters[1:]:
+        string = string.replace(i, final_delimiter)
+    return string.split(final_delimiter)
 
-    for delimiter in delimiters:
-        for i, substring in enumerate(stack):
-            substack = substring.split(delimiter)
-            stack.pop(i)
-            for j, _substring in enumerate(substack):
-                stack.insert(i+j, _substring)
-
-    return stack
 
 def schunk(string, size):
     """Splits string into n sized chunks."""
-
-    stack = []
-
-    substack = []
-    current_count = 0
-
-    for char in string:
-        if not current_count < size:
-            stack.append(''.join(substack))
-            substack = []
-            current_count = 0
-
-        substack.append(char)
-        current_count += 1
-
-    if len(substack):
-        stack.append(''.join(substack))
-
-    return stack
+    return [string[i:i+size] for i in range(0, len(string), size)]
