73b1bdd2d444084d121ce301391fdf1359f3e1b9
Jason Piper <j.piper@me.com>
2014-01-28 23:17:46 +0000

    0.3.4

--- clint/__init__.py
+++ clint/__init__.py
@@ -26,8 +26,8 @@ from .pipes import piped_in
 
 
 __title__ = 'clint'
-__version__ = '0.3.3'
-__build__ = 0x000303
+__version__ = '0.3.4'
+__build__ = 0x000304
 __author__ = 'Kenneth Reitz'
 __license__ = 'ISC'
 __copyright__ = 'Copyright 2012 Kenneth Reitz'
