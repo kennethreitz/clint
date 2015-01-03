1c1f1c93f947e442a1f6d8122c31d747544a5775
dstrohl <Kur1tacams>
2015-01-02 21:32:42 -0800

    Added option prompt

--- examples/prompt.py
+++ examples/prompt.py
@@ -15,7 +15,14 @@ if __name__ == '__main__':
     # Set validators to an empty list for an optional input
     language = prompt.query("Your favorite tool (optional)?", validators=[])
 
+    # Shows a list of options to select from
+    inst_options = [{'selector':'1','prompt':'Full','return':'full'},
+                    {'selector':'2','prompt':'Partial','return':'partial'},
+                    {'selector':'3','prompt':'None','return':'no install'}]
+    inst = prompt.options("Full or Partial Install", inst_options)
+
     # Use a default value and a validator
     path = prompt.query('Installation Path', default='/usr/local/bin/', validators=[validators.PathValidator()])
 
-    puts(colored.blue('Hi {0}. Install {1} to {2}'.format(name, language or 'nothing', path)))
+    puts(colored.blue('Hi {0}. Install {1} {2} to {3}'.format(name, inst, language or 'nothing', path)))
+
