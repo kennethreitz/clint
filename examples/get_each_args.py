7865d7a37562be8b0af9b3668043d8c08138814b
Hung Nguyen Viet <hvn@familug.org>
2017-02-03 21:02:18 +0700

    Add clint to import paths

--- examples/get_each_args.py
+++ examples/get_each_args.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+
 from clint.arguments import Args
 from clint.textui import puts, colored
 
